d13ad9148e78f2c03ef2cd6a28861b88b23e7e7f
Nikita <n.oszejko27@gmail.com>
2023-05-09 22:06:41 +0200

    improving the admin panel and changing who_is

--- v2/licznik_czasu/admin.py
+++ v2/licznik_czasu/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm
 from .models import Client, Project, Employee, Task, TaskTimer, User
 
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'who_is')
     add_fieldsets = (
         (None, {
@@ -13,10 +16,21 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
 
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Custom fields', {'fields': ('who_is',)}),
+    )
+
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('project_name', 'description', 'client')
+    list_display = ('project_name', 'project_description', 'client')
+
+    def project_description(self, obj):
+        return obj.description[:30] + '...' if len(obj.description) > 30 else obj.description
 
 
 @admin.register(Client)
@@ -31,7 +45,10 @@ class EmployeeAdmin(admin.ModelAdmin):
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('task_name', 'description', 'creation_date', 'project')
+    list_display = ('task_name', 'task_description', 'creation_date', 'project')
+
+    def task_description(self, obj):
+        return obj.description[:30] + '...' if len(obj.description) > 30 else obj.description
 
 
 @admin.register(TaskTimer)
